982e545f5e505993c95123022b3613b4187cd2f6
pruebaforme <pruebaforme@gmail.com>
2012-02-28 17:44:28 +0100

    Resolviendo problema con ruta/repository

--- src/Comunidad/UsersBundle/Controller/DefaultController.php
+++ src/Comunidad/UsersBundle/Controller/DefaultController.php
@@ -7,11 +7,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    
+     public function indexAction()
+    {
+	
+        return $this->render('UsersBundle:Default:index.html.twig');
+    }
+
+
     public function listaInmueblesAction()
     {
 	$em = $this->getDoctrine()->getEntityManager();
-	$inmuebles = $em->getRepository('UsersBundle:Inmuebles')->findListaInmuebles();
+	$inmuebles = $em->getRepository('UsersBundle:Inmueble')->findListaInmuebles();
 
 	
         return $this->render('UsersBundle:Default:listaInmuebles.html.twig', array('inmuebles' => $inmuebles));
